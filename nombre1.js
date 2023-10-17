2bddd38bb9e4329e18fc2001d3390a2d3c72189d
juancagu79 <juanzo28@hotmail.com>
2023-10-17 08:00:17 -0500

    cambio de 17-10-2023

---
+++ nombre1.js
@@ -0,0 +1,1 @@
+const nombre1 = "Marta”;";
---
+++ script.js
@@ -0,0 +1,11 @@
+//alert("Hola este es mi Javascript");
+
+let nombre = "Marta";
+nombre = "Maria";
+var nombre1 = "Marta";
+
+const nombre2 = "Marta";
+
+console.log(nombre);
+console.log(nombre1);
+console.log(nombre2);
